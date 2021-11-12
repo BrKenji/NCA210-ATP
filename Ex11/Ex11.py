ebb7738ff2d42661c09ccdd0d86dbc532836b461
BrKenji <ninihiratafei@gmail.com>
2021-11-11 22:49:23 -0300

    Question 2 from Ex11

--- Ex11/Ex11.py
+++ Ex11/Ex11.py
@@ -15,3 +15,30 @@ leg = axis.legend()
 axis.legend(loc='upper left', ncol=2,shadow=True, borderpad=1, frameon=False)
 plt.show()
 """
+## Ex2
+"""
+df = pd.read_csv("./SP500_prices_monthly.csv")
+df_list = []
+year_list = []
+mean_prices_list = []
+
+year_start = 2000
+year_end = 2010
+
+# Fills year_list with the years used on the plot
+for i in range(year_start, year_end + 1) :
+    year_list.append(str(i))
+# Fills df_list with the dataframes of each year
+for year in year_list:
+    df_list.append(df[df["Date"].str.contains(year)])
+# Fills mean_prices_list with the mean prices of each dataframe
+for dataframe in df_list:
+    mean_prices_list.append(dataframe["Price"].mean())
+
+fig, axis = plt.subplots()
+plt.bar(year_list, mean_prices_list)
+plt.title("Média Anual do Preço Histórioco da Bolsa de NY")
+plt.ylabel("Preço (USD)")
+plt.xlabel("Ano")
+plt.show()
+"""
